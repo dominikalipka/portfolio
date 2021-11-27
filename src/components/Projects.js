b32d728953b8e758585d49146423ed4f353f85ca
Dominika Lipka <dominika.lipka96@gmail.com>
2021-11-27 18:50:06 +0100

    Added information about technologies used in projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,41 +2,56 @@ import React from "react";
 
 function Projects() {
   return (
-    <div>
+    <div className="projects">
       <div className="projects-title">
         <h2>My awesome projects</h2>
         <div className="text-underline"></div>
       </div>
-      <div className="project-container">
+      <div className="project-container-1">
         <div className="project-description">
           <h3>juniors4seniors</h3>
           <h4>Technologies used:</h4>
+          <div className="technologies">
+            <p>React.js</p>
+            <p>JavaScript</p>
+            <p>HTML & CSS</p>
+            <p>REST API</p>
+            <p>Node.js</p>
+            <p>MongoDB</p>
+            <p>Mongoose</p>
+          </div>
           <h5>About the project:</h5>
         </div>
         <div className="project-screenshots"></div>
       </div>
-      <div className="project-container">
+      <div className="project-container-2">
         <div className="project-screenshots"></div>
         <div className="project-description">
           <h3>SPACE SNEK</h3>
           <h4>Technologies used:</h4>
+          <div className="technologies">
+            <p>JavaScript - OOP & DOM</p>
+            <p>HTML & CSS</p>
+          </div>
           <h5>About the project:</h5>
         </div>
       </div>
-      <div className="project-container">
+      <div className="project-container-1">
         <div className="project-description">
           <h3>plantin'</h3>
           <h4>Technologies used:</h4>
+          <div className="technologies">
+            <p>JavaScript</p>
+            <p>HTML & CSS</p>
+            <p>Node.js</p>
+            <p>MongoDB</p>
+            <p>Mongoose</p>
+            <p>Express.js</p>
+          </div>
           <h5>About the project:</h5>
         </div>
         <div className="project-screenshots"></div>
       </div>
-      {/* 
-            About each prodject - small description, technologies, links to repos and demo:
-            - Space Snek
-            - plantin'
-            - juniors4seniors
-             */}
     </div>
   );
 }
