2cac7574a2c0ca2b1a69fc01f310d8274eb225eb
Dominika Lipka <dominika.lipka96@gmail.com>
2021-11-27 19:13:14 +0100

    added screenshot and demo button to first project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import juniors4seniors1 from '../images/juniors4seniors-1.png'
+import juniors4seniors2 from "../images/juniors4seniors-2.png";
+import juniors4seniors3 from "../images/juniors4seniors-3.png";
+
 function Projects() {
   return (
     <div className="projects">
@@ -10,6 +14,13 @@ function Projects() {
       <div className="project-container-1">
         <div className="project-description">
           <h3>juniors4seniors</h3>
+          <a
+            className="links"
+            href="https://juniors4seniors.netlify.app/"
+            target="_blank"
+          >
+             DEMO  
+          </a>
           <h4>Technologies used:</h4>
           <div className="technologies">
             <p>React.js</p>
@@ -22,7 +33,11 @@ function Projects() {
           </div>
           <h5>About the project:</h5>
         </div>
-        <div className="project-screenshots"></div>
+        <div className="project-screenshots">
+          <img src={juniors4seniors1} />
+          <img src={juniors4seniors2} />
+          <img src={juniors4seniors3} />
+        </div>
       </div>
       <div className="project-container-2">
         <div className="project-screenshots"></div>
